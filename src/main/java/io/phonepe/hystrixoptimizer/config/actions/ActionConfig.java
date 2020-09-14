1326bd48d1cca57e86943af7f1e573112e82b0c2
Deepak Kumar Singh <deepakks1595@gmail.com>
2020-09-15 00:23:02 +0530

    added json subtype and json info

--- src/main/java/io/phonepe/hystrixoptimizer/config/actions/ActionConfig.java
+++ src/main/java/io/phonepe/hystrixoptimizer/config/actions/ActionConfig.java
@@ -1,5 +1,9 @@
 package io.phonepe.hystrixoptimizer.config.actions;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import io.phonepe.hystrixoptimizer.config.actions.impl.EmailConfig;
+import io.phonepe.hystrixoptimizer.config.actions.impl.UpdateHystrixConfig;
 import io.phonepe.hystrixoptimizer.models.ActionType;
 import io.phonepe.hystrixoptimizer.utils.ActionTypeVisitor;
 import lombok.Data;
@@ -7,6 +11,11 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "actionType")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = UpdateHystrixConfig.class, name = "UPDATE_HYSTRIX_CONFIG"),
+        @JsonSubTypes.Type(value = EmailConfig.class, name = "SEND_EMAIL_ALERT")
+})
 public abstract class ActionConfig {
 
     private ActionType actionType;
